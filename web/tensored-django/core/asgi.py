f2cb33822b42b7eb63eaee75371e349f1e1f347d
HummusDeArialux <128862707+HummusDeArialux@users.noreply.github.com>
2024-01-12 17:26:36 +0100

    Commented asgi.py

--- web/tensored-django/core/asgi.py
+++ web/tensored-django/core/asgi.py
@@ -7,10 +7,14 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 """
 
+# Import necessary modules
 import os
 
+# Import the ASGI application from Django
 from django.core.asgi import get_asgi_application
 
+# Set the default Django settings module to 'core.settings'
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
+# Create the ASGI application using Django's get_asgi_application function
 application = get_asgi_application()
