c800456a0a27cfbb810202f91c28bea4787cb268
HummusDeArialux <128862707+HummusDeArialux@users.noreply.github.com>
2024-01-12 16:36:11 +0100

    Commented manage.py

--- web/tensored-django/manage.py
+++ web/tensored-django/manage.py
@@ -1,22 +1,33 @@
 #!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
+"""
+Django's command-line utility for administrative tasks.
+This script serves as the main entry point for running Django management commands.
+"""
 import os
 import sys
 
 
 def main():
-    """Run administrative tasks."""
+    """
+    Main function to run administrative tasks using Django's command-line utility.
+    """
+
+    # Set the default Django settings module to 'core.settings'
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
     try:
+        # Try to import the execute_from_command_line function from Django's core management module
         from django.core.management import execute_from_command_line
     except ImportError as exc:
+        # Handle ImportError if Django is not installed or not available
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+        
+    # Execute Django management commands from the command line arguments
     execute_from_command_line(sys.argv)
 
-
+# Entry point: Run the main function if this script is executed directly
 if __name__ == '__main__':
     main()
