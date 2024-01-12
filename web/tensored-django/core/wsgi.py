ff5fb60a70a6f28cdebbae1e50a1b25cbc69a996
HummusDeArialux <128862707+HummusDeArialux@users.noreply.github.com>
2024-01-12 17:29:54 +0100

    commented wsgi.py

--- web/tensored-django/core/wsgi.py
+++ web/tensored-django/core/wsgi.py
@@ -7,10 +7,14 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 
+# Import necessary modules
 import os
 
+# Import the WSGI application from Django
 from django.core.wsgi import get_wsgi_application
 
+# Set the default Django settings module to 'core.settings'
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
+# Create the WSGI application using Django's get_wsgi_application function
 application = get_wsgi_application()
